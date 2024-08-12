5e9ae5b74ee1c5e26b85f10eb071b686d4c7a406
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-08-12 12:48:39 +0300

    Add project files.

---
+++ MauiBaseProject/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBaseProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiBaseProject/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBaseProject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
